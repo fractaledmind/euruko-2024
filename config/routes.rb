c77addbd6302829931733228493831bd59cd4d72
Stephen Margheim <stephen.margheim@gmail.com>
2024-09-04 19:56:47 +0200

    Scaffold Session resource
    Console logs:
    
    ```
    $ bin/rails generate scaffold Session user:belongs_to user_agent:string ip_address:string
          invoke  active_record
          create    db/migrate/20240904175617_create_sessions.rb
          create    app/models/session.rb
          invoke    test_unit
          create      test/models/session_test.rb
          create      test/fixtures/sessions.yml
          invoke  resource_route
           route    resources :sessions
          invoke  scaffold_controller
          create    app/controllers/sessions_controller.rb
          invoke    tailwindcss
          create      app/views/sessions
          create      app/views/sessions/index.html.erb
          create      app/views/sessions/edit.html.erb
          create      app/views/sessions/show.html.erb
          create      app/views/sessions/new.html.erb
          create      app/views/sessions/_form.html.erb
          create      app/views/sessions/_session.html.erb
          invoke    resource_route
          invoke    test_unit
          create      test/controllers/sessions_controller_test.rb
          create      test/system/sessions_test.rb
          invoke    helper
          create      app/helpers/sessions_helper.rb
          invoke      test_unit
    ```

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sessions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[ show new create ]
 
